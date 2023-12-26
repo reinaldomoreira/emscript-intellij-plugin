96e004ed84799c051bc9ed51b83f5facdc7f9838
Reinaldo <rms_master@hotmail.com>
2023-12-26 17:51:11 -0300

    rename package

---
+++ src/main/gen/emscriptintellijplugin/psi/SimpleProperty.java
@@ -0,0 +1,23 @@
+// This is a generated file. Not intended for manual editing.
+package emscriptintellijplugin.psi;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.navigation.ItemPresentation;
+
+import emscriptintellijplugin.psi.SimpleNamedElement;
+
+public interface SimpleProperty extends SimpleNamedElement {
+
+  String getKey();
+
+  String getValue();
+
+  String getName();
+
+  PsiElement setName(String newName);
+
+  PsiElement getNameIdentifier();
+
+  ItemPresentation getPresentation();
+
+}
---
+++ src/main/java/emscriptintellijplugin/SimpleLexerAdapter.java
@@ -0,0 +1,15 @@
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package emscriptintellijplugin;
+
+import com.intellij.lexer.FlexAdapter;
+
+import emscriptintellijplugin.SimpleLexer;
+
+public class SimpleLexerAdapter extends FlexAdapter {
+
+  public SimpleLexerAdapter() {
+    super(new SimpleLexer(null));
+  }
+
+}
