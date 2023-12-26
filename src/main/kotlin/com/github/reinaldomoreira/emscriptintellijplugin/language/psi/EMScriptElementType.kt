6a535ad2005c6559968e5537ae5de99c34323ab2
Reinaldo <rms_master@hotmail.com>
2023-12-26 17:35:23 -0300

    remove EMS code, add Simple code

--- src/main/kotlin/com/github/reinaldomoreira/emscriptintellijplugin/language/psi/EMScriptElementType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.reinaldomoreira.emscriptintellijplugin.language.psi
-
-import com.github.reinaldomoreira.emscriptintellijplugin.language.EMScriptLanguage
-import com.intellij.psi.tree.IElementType
-import org.jetbrains.annotations.NonNls
-
-class EMScriptElementType(debugName: @NonNls String) : IElementType(debugName, EMScriptLanguage.INSTANCE)
